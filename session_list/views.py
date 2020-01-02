d40461053ecd7f91fb3e122f25d8e83a015ff86a
kiarn <arnaud@kientz.me>
2020-01-02 16:27:53 +0100

    Right function name.

--- session_list/views.py
+++ session_list/views.py
@@ -14,7 +14,7 @@ class Handler(HttpPlugin):
 ## Check rights
     @url(r'/api/session_list/list')
     @endpoint(api=True)
-    def handle_api_check_cert(self, http_context):
+    def handle_api_list_sessions(self, http_context):
         if http_context.method == 'GET':
             self.context.worker.update_sessionlist()
             gevent.sleep(1)
